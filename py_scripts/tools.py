850ef2087d2651da3b66b83ea567847a61cefd3f
sdonne <stefanodonne@gmail.com>
2021-12-29 15:12:32 +0100

    Continue data preparation

--- py_scripts/tools.py
+++ py_scripts/tools.py
@@ -1,15 +1,16 @@
 # File containing function used for data preparation
 
+# TODO : adds proper doc
 
-def filters_mean(row, data_frame, feature_name, list_features):
+def filters_mean(row, data_frame, feature_name, list_features, numeric):
     bool_val = [data_frame[feature] == row[feature] for feature in list_features]
     bool_ = bool_val[0]
     for i in range(len(list_features) - 1): bool_ = bool_ & bool_val[i + 1]
     filtered_df = data_frame[bool_]
-    return filtered_df[feature_name].mean()
+    return filtered_df[feature_name].mean() if numeric else filtered_df[feature_name].value_counts().index[0]
 
 
-def mean_feature_clustered_numeric(data_frame, feature_name, list_features):
+def mean_feature_clustered(data_frame, feature_name, list_features, numeric):
     """
     It returns a data_frame where the NaN value of the given 'feature_name' are replaced by the
     mean values of the 'feature_name' that match the given filters of list_features
@@ -19,6 +20,7 @@ def mean_feature_clustered_numeric(data_frame, feature_name, list_features):
     :return: updated data_frame
     """
     lnan = data_frame[data_frame[feature_name].isna()]
-    lnan[feature_name] = lnan.apply(lambda row: filters_mean(row, data_frame, feature_name, list_features), axis=1)
+    lnan[feature_name] = lnan.apply(lambda row: filters_mean(row, data_frame, feature_name, list_features, numeric),
+                                    axis=1)
     data_frame.loc[lnan.respondent_id, feature_name] = lnan[feature_name]
     return data_frame
